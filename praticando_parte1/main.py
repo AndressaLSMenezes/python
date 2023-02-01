338b39b6c8520b2ffa88a712f88c67fec194b234
Andressa Lopes <andressalsmenezes@gmail.com>
2023-01-31 23:21:31 -0300

    praticando_parte1

---
+++ praticando_parte1/main.py
@@ -0,0 +1,17 @@
+# main.py
+from operacoes_matematicas.soma import somar 
+from operacoes_matematicas.subtracao import subtrair
+from operacoes_matematicas.multiplicacao import multiplicar
+
+
+resultado_da_multiplicacao = multiplicar(4, 5)
+print(resultado_da_multiplicacao)
+# 20
+
+resultado_da_soma = somar(15, 7)
+print(resultado_da_soma)
+# 22
+
+resultado_da_subtracao = subtrair(15, 7)
+print(resultado_da_subtracao)
+# 8
